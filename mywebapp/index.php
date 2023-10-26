5fada1a3646b9bf127e0b7d9ce1c811e4ff5e428
balajikendey <87965657+balajikendey@users.noreply.github.com>
2023-10-26 15:02:07 +0530

    Update index.php

--- mywebapp/index.php
+++ mywebapp/index.php
@@ -4,7 +4,7 @@
   </head>
   <body>
     <?php
-      $conn = new mysqli("sql_container", "root", kinddevil);
+      $conn = new mysqli("sql_container", "root");
       if($conn->connect_error){
         echo "\nError: Unable to connect: " . $conn->connect_error;
       }
